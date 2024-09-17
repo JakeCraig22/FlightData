0638936a07aef1c52a4472ee3d01bf176f9c09db
JakeCraig22 <159073970+JakeCraig22@users.noreply.github.com>
2024-09-16 23:24:36 -0400

    Create Airport.java

---
+++ Airport.java
@@ -0,0 +1,52 @@
+/**
+ * @author Jacob Craig & Troy Moscatelli
+ * @version 9/13/2024
+ * Initializes and stores variables 
+ */
+public class Airport {
+	
+	private String name;
+	private String city;
+	private String state;
+	
+	
+	/**
+	 * @param name - name of airport
+	 * @param city - airport the city is in
+	 * @param state - airport the state is in
+	 */
+	public Airport(String name, String city, String state) {
+		this.name = name;
+		this.city = city;
+		this.state = state; 
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public String getAirportName() {
+		return name;
+	}
+	public String getCityName() {
+		return city;
+	}
+	public String getStateName() {
+		return state;
+	}
+	
+	 @Override
+	    public String toString() {
+	        return name + ", " + city + ", " + state;
+	    }
+	
+}
